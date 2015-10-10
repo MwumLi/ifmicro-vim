619bd6832c0756c9f93ee96d1ac806bd997022ec
mwumli <mwumli@hotmail.com>
2015-10-10 23:11:40 +0800

    add install.sh and perfect README.md

--- README.md
+++ README.md
@@ -14,7 +14,36 @@ So I can't sure that whether it take effects for you in working platform.
 
 But you can try it, don't worry it will  make a bad influence for you.
 
+## Quick install
+
+    $ git clone -b ifmicro_vim https://www.github.com/MwumLi/ifmicro_code.git 
+    $ cd ifmicro_code && ./install.sh
+    
+## Load order
+
+`.vimrc` is a controller, which control load order  
+It load `.vimrc-plugins -> .vimrc-custom -> .vimrc-funcs -> .vimrc-keys` in order
+
+1. `.vimrc.plugins`  
+vim plugins and them configuration, key maps  
+
+2. `.vimrc.custom`  
+vim's configuration , not belong plugins  
+
+3. `.vimrc.funcs`
+define some  function by vim and it's plugins feature
+
+4. `.vimrc.keys`
+all key maps , including apperance before vimrc*
+
+## option
+
+1. `g:ifmicro_ycm_enable`  
+
+if you need YouCompleteMe to completion, you must be : `let g:ifmicro_ycm_enable=1`  
 
 
+2. `g:ifmicro_dark_color_tone_enable`  
 
+if you need dark color tone, you must be : `let g:ifmicro_dark_color_tone_enable=1`  
 
---
+++ install.sh
@@ -0,0 +1,160 @@
+#!/usr/bin/env bash
+
+# all old configurations
+backup_configs=('.vimrc' '.vim' '.vimrc.plugin' '.vimrc.custom' '.vimrc.funcs' '.vimrc.keys')
+# all new configuration
+vimrcs=("vimrc" "vimrc.plugins" "vimrc.custom" "vimrc.funcs" "vimrc.keys")
+# vim user dirctory
+vim_dir=("bundle" "colors" "autoload" "doc" "ftplugin" "syntax" "plugin")
+cur_position=`pwd`
+# the project path -- must be absolute path
+# if the install.sh isn't project, you must specify a ifmicro_vim path
+ifmicro_vim=`pwd`
+ifmicro_vim='/home/vagrant/synced_folder/WorkSpace/ifmicro-code/ifmicro-vim'
+# install path , the default is $HOME
+root_dir="$HOME/c++/test"
+
+function msg() {
+    printf '%b\n' "$1" >&2
+}
+function success() {
+    
+    msg_str="\33[32m[✔]\33[0m ${1}"
+
+    if [ $# == 2 ]; then 
+        msg_str="${msg_str} : ${2}"
+    fi 
+
+    msg "${msg_str}"
+}
+
+function error()
+{
+    msg_str="\33[31m[✘]\33[0m ${1}"
+
+    if [ $# == 2 ]; then 
+        msg_str="${msg_str} : ${2}"
+    fi 
+
+    msg "${msg_str}"
+}
+
+function print_curtime() {
+    echo `date '+%F %r'`
+    echo
+}
+
+# .vimrc .vim 
+function backup() {
+    
+    length=${#backup_configs[*]}
+
+
+    today=`date '+%Y%m%d_%s'`
+
+    mkdir -vp "${root_dir}/vim.${today}" 1>>log.txt 2>>log.txt
+    print_curtime >>log.txt
+
+    msg "Attempting to back up your old vim configuration."
+    for (( i = 0; i < ${length}; i++ )); do
+        per_config="${root_dir}/${backup_configs[$i]}"
+
+        if [ -e "${per_config}" ] && [ ! -L "${per_config}" ]; then 
+        #if [ -e "${per_config}" ]; then 
+            cp -Rv "${per_config}" "${root_dir}/vim.${today}/"  1>>log.txt 2>>log.txt
+             print_curtime >>log.txt
+
+            if [ "$?" != "0" ]; then 
+                error "${per_config} backup failed"
+                error "exit and abort installation"
+                exit 1
+            fi
+        else
+            # path doesn't exist
+            echo  "$original_file doesn't exist or is a link" 1>>log.txt 2>>log.txt
+        fi
+    done
+    success "Your original vim configuration has been backed up." 
+
+}
+
+# backup
+function config() {
+ 
+   # build links
+    msg "start to build links"
+    length=${#vimrcs[*]}
+    for (( i = 0; i < ${length}; i++ )); do
+        ln -sfv "${ifmicro_vim}/${vimrcs[$i]}" "${root_dir}/.${vimrcs[$i]}" 1>>log.txt 2>>log.txt
+        print_curtime >>log.txt
+
+        if [ "$?" != "0" ]; then 
+            error "${root_dir}/${vimrcs[$i]} symolic link failed"
+            error "exit and abort installation"
+            exit 1
+        fi
+    done 
+    success "build links successfully"
+
+    # create vim resource dirs
+    msg "start to create vim resource directories"
+    length=${#vim_dir[*]}
+    for (( i = 0; i < ${length}; i++ )); do
+        mkdir -vp "${root_dir}/.vim/${vim_dir[$i]}" 1>>log.txt 2>>log.txt
+        print_curtime >>log.txt
+    done
+    success "create vim resource successfully"
+
+    # download Vundle for managing other vim plugins
+    msg "satrt to download Vundle.vim"
+    git clone https://github.com/gmarik/Vundle.vim.git ${root_dir}/.vim/bundle/Vundle.vim 1>>log.txt 2>>log.txt
+    print_curtime >>log.txt
+    if [ "$?" = "0" ]; then
+        success "downloading Vundle.vim successfully"
+    else
+        error "Vundle failed" " Try to : git clone https://github.com/gmarik/Vundle.vim.git ${root_dir}/.vim/bundle/Vundle.vim"
+    fi
+
+    # build simplified Chinese help document
+    msg "start to build simplified chinese help document"
+    tar -xvf ${ifmicro_vim}/vimcdoc-1.9.0.tar.gz -C ${ifmicro_vim}/ 1>>log.txt 2>>log.txt
+    print_curtime >>log.txt
+
+    if [ "$?" = "0" ]; then 
+        cd ${ifmicro_vim}/vimcdoc-1.9.0 && ./vimcdoc.sh -I 1>>log.txt 2>>log.txt
+        if [ "$?" = "0" ] ; then    
+            # delete simplifie chinese doc
+            cd -
+            rm -rv ${ifmicro_vim}/vimcdoc-1.9.0 1>>log.txt 2>>log.txt
+            print_curtime >>log.txt
+
+            success "Chinese-Vim-Doc is installed successfully!" "Try to ':help vim' in vim Normal mode"
+        else
+            error "Chines-Vim-Doc failed" "Try to download from github"
+        fi
+    else 
+        error "Chines-Vim-Doc failed" "Try to download from github"
+    fi
+
+
+    # install colorscheme 
+    msg "start to move colorscheme"
+    cp -Rv ${ifmicro_vim}/colors/*  ${root_dir}/.vim/colors/ 1>>log.txt 2>>log.txt
+    if [ "$?" = "0" ]; then
+        success "Colorscheme is placed below ${root_dir}/.vim/colors successfully"
+    else
+        error "ColorScheme" "Try to search"
+    fi
+    
+    vim +PluginInstall +qall
+}
+cd ${ifmicro_vim}
+msg "ifmicro_vim will be installed below  ${root_dir}"
+
+backup
+config
+
+success "ifmicro_vim installed successfully"
+msg     "Thanks for installing ifmicro_vim"
+msg     "© `date +%Y` http://www.ifmicro.com/"
+cd ${cur_position}
